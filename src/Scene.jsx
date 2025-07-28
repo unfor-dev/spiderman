069aaf1be028fd6dd0a583ad6c6db0d29af13f3b
unfor-dev <hamzayevasadulloh2005@gmail.com>
2025-07-28 23:15:47 +0500

    Adding project starter

---
+++ src/Scene.jsx
@@ -0,0 +1,8 @@
+export default function Scene() {
+  return (
+    <div className="scene">
+      <h1>Spiderman by Unfor</h1>
+      {/* Other components or elements can be added here */}
+    </div>
+  );
+}
